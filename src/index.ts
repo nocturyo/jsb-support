c8b37601398babca3f0b701a4ba44cf036aee694
nocturyo00 <artiomkaczanowski@hotmail.com>
2025-08-27 18:29:03 +0200

    dodano 2 komendy

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,20 @@ import ping from './commands/ping.js';
 import userinfo from './commands/userinfo.js';
 import serverinfo from './commands/serverinfo.js';
 
+
 const client = new ExtendedClient();
 
+
+// Rejestrujemy komendy w pamięci klienta
 client.commands.set(ping.data.name, ping);
 client.commands.set(userinfo.data.name, userinfo);
 client.commands.set(serverinfo.data.name, serverinfo);
 
+
+// Podpinamy eventy
 ready(client);
 interactionCreate(client);
 
+
+// Logowanie bota
 client.login(process.env.DISCORD_TOKEN);
